b4207bf45b1e4c625ce78ae34ccff93fc2c224d5
Andres Faraone <andresfaraone@outlook.com>
2018-11-18 16:16:27 +0000

    GildedRose calls items to update themselves
    Now it no longer asks dependencies about its state
    it tells them to carry out their behaviour

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,10 +1,18 @@
 require_relative '../gilded_rose'
 
 describe GildedRose do
+  let(:mock_item_one) { double(:mock_item_one)}
+  let(:mock_item_two) { double(:mock_item_two)}
+  let(:mock_items) { [mock_item_one, mock_item_two] }
+  subject { described_class.new(mock_items)}
 
   describe "#update_quality" do
-    let(:mock_item) { double(:mock_item, name: "foo", sell_in: 0, quality: 0)}
-    let(:mock_items) { [mock_item] }
-    subject { described_class.new(mock_items)}
+    it 'calls #update_item on each item' do
+      mock_items.each do |item|
+        allow(item).to receive(:update_item)
+        expect(item).to receive(:update_item)
+      end
+      subject.update_quality
+    end
   end
 end
